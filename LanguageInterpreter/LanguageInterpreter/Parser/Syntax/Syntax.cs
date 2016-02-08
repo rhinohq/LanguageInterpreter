16c49d96b502dd92dfaebcfe216d8cfe7b6878ee
Ryan Kendrick <ryan@kendrickfamily.info>
2016-02-08 18:14:45 +0000

    Interpreter Overhaul

---
+++ LanguageInterpreter/LanguageInterpreter/Parser/Syntax/Syntax.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace LanguageInterpreter.Parser.Syntax
+{
+    class Syntax
+    {
+        public partial class Access
+        {
+        }
+
+        public partial class Args
+        {
+            public List<Expr> ArgList = new List<Expr>();
+
+            public StringLiteral String;
+
+            public TableConstructor Table;
+        }
+
+        public partial class Assignment : Statement
+        {
+            public List<Var> VarList = new List<Var>();
+
+            public List<Expr> ExprList = new List<Expr>();
+        }
+
+        public partial class BaseExpr
+        {
+        }
+
+        public partial class BoolLiteral : Term
+        {
+            public string Text;
+        }
+
+        public partial class BreakStmt : Statement
+        {
+        }
+
+        public partial class Chunk
+        {
+            public List<Statement> Statements = new List<Statement>();
+        }
+
+        public partial class DoStmt : Statement
+        {
+            public Chunk Body;
+        }
+
+        public partial class ElseifBlock
+        {
+            public Expr Condition;
+
+            public Chunk ThenBlock;
+        }
+
+        public partial class Expr
+        {
+        }
+
+        public partial class ExprStmt : Statement
+        {
+            public Expr Expr;
+        }
+
+        public partial class Field
+        {
+            public Expr Value;
+        }
+
+        public partial class ForInStmt : Statement
+        {
+            public List<string> NameList = new List<string>();
+
+            public List<Expr> ExprList = new List<Expr>();
+
+            public Chunk Body;
+        }
+
+        public partial class ForStmt : Statement
+        {
+            public string VarName;
+
+            public Expr Start;
+
+            public Expr End;
+
+            public Expr Step;
+
+            public Chunk Body;
+        }
+
+        public partial class Function : Statement
+        {
+            public FunctionName Name;
+
+            public FunctionBody Body;
+        }
+
+        public partial class FunctionBody
+        {
+            public ParamList ParamList;
+
+            public Chunk Chunk;
+        }
+
+        public partial class FunctionCall : Access
+        {
+            public Args Args;
+        }
+
+        public partial class FunctionName
+        {
+            public List<string> FullName = new List<string>();
+
+            public string MethodName;
+        }
+
+        public partial class FunctionValue : Term
+        {
+            public FunctionBody Body;
+        }
+
+        public partial class GroupExpr : BaseExpr
+        {
+            public Expr Expr;
+        }
+
+        public partial class IfStmt : Statement
+        {
+            public Expr Condition;
+
+            public Chunk ThenBlock;
+
+            public List<ElseifBlock> ElseifBlocks = new List<ElseifBlock>();
+
+            public Chunk ElseBlock;
+        }
+
+        public partial class ItemValue : Field
+        {
+        }
+
+        public partial class KeyAccess : Access
+        {
+            public Expr Key;
+        }
+
+        public partial class KeyValue : Field
+        {
+            public Expr Key;
+        }
+
+        public partial class LocalFunc : Statement
+        {
+            public string Name;
+
+            public FunctionBody Body;
+        }
+
+        public partial class LocalVar : Statement
+        {
+            public List<string> NameList = new List<string>();
+
+            public List<Expr> ExprList = new List<Expr>();
+        }
+
+        public partial class MethodCall : Access
+        {
+            public string Method;
+
+            public Args Args;
+        }
+
+        public partial class NameAccess : Access
+        {
+            public string Name;
+        }
+
+        public partial class NameValue : Field
+        {
+            public string Name;
+        }
+
+        public partial class NilLiteral : Term
+        {
+        }
+
+        public partial class NumberLiteral : Term
+        {
+            public string HexicalText;
+
+            public string Text;
+        }
+
+        public partial class OperatorExpr : Expr
+        {
+        }
+
+        public partial class ParamList
+        {
+            public List<string> NameList = new List<string>();
+
+            public bool HasVarArg;
+
+            public string IsVarArg;
+        }
+
+        public partial class PrimaryExpr : Term
+        {
+            public BaseExpr Base;
+
+            public List<Access> Accesses = new List<Access>();
+        }
+
+        public partial class RepeatStmt : Statement
+        {
+            public Chunk Body;
+
+            public Expr Condition;
+        }
+
+        public partial class ReturnStmt : Statement
+        {
+            public List<Expr> ExprList = new List<Expr>();
+        }
+
+        public partial class Statement
+        {
+        }
+
+        public partial class StringLiteral : Term
+        {
+            public string Text;
+        }
+
+        public partial class TableConstructor : Term
+        {
+            public List<Field> FieldList = new List<Field>();
+        }
+
+        public partial class Term : Expr
+        {
+        }
+
+        public partial class Var
+        {
+            public BaseExpr Base;
+
+            public List<Access> Accesses = new List<Access>();
+        }
+
+        public partial class VariableArg : Term
+        {
+            public string Name;
+        }
+
+        public partial class VarName : BaseExpr
+        {
+            public string Name;
+        }
+
+        public partial class WhileStmt : Statement
+        {
+            public Expr Condition;
+
+            public Chunk Body;
+        }
+    }
+}
--- LanguageInterpreter/LanguageInterpreter/Program.cs
+++ LanguageInterpreter/LanguageInterpreter/Program.cs
@@ -1,7 +1,4 @@
-﻿// Program entry point.
-
-using System;
-using System.IO;
+﻿using System;
 
 namespace LanguageInterpreter
 {
@@ -9,34 +6,7 @@ namespace LanguageInterpreter
     {
         private static void Main(string[] args)
         {
-            // If the expected command line arguments have not been given, it prints the usage guide.
-            if (args.Length != 2)
-                Usage();
-
-            // Read all the source code from specified file.
-            string Code = File.ReadAllText(args[1]);
-
-            // If Lua is picked, it runs the Lua interpreter.
-            if (args[0] == "/l")
-                InterpretLua(Code);
-        }
-
-        // The Usage Guide
-        public static void Usage()
-        {
-            Console.WriteLine("Usage: <Language Choice> <Source Code Filename>");
-            Console.WriteLine("Lua: /l");
-            Console.WriteLine("Python: /p");
-            Environment.Exit(0);
-        }
-
-        // Lua Interpreter
-        public static void InterpretLua(string Code)
-        {
-            Lua.LuaLexer Lexer = new Lua.LuaLexer();
-            string[,] Tokens = Lexer.LuaLex(Code);
-
-            Lua.LuaParser Parser = new Lua.LuaParser();
+            
         }
     }
 }
